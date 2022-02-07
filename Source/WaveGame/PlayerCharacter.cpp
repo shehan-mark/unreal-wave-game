f3b99a4632343a0ee89e51d2321e8d1325b13c05
Shehan Neomal Mark Fonseka <markfonseka.z@gmail.com>
2022-02-07 09:34:14 +0530

    player controller pitch and yaw limitations

--- Source/WaveGame/PlayerCharacter.cpp
+++ Source/WaveGame/PlayerCharacter.cpp
@@ -59,7 +59,7 @@ void APlayerCharacter::MoveForward(float Value)
 {
 	if ((Controller != nullptr) && (Value != 0.0f))
 	{
-		FRotator Rotation = Controller->GetControlRotation(); // getting forward rotation/ vector relative to world direction
+		FRotator Rotation = Controller->GetControlRotation();
 		//UE_LOG(LogTemp, Warning, TEXT("Current Rotation %f - %f - %f"), Rotation.Vector().X, Rotation.Vector().Y, Rotation.Vector().Z);
 
 		FRotator NewYawRotation(0, Rotation.Yaw, 0); // this confuses me
@@ -70,6 +70,7 @@ void APlayerCharacter::MoveForward(float Value)
 		UE_LOG(LogTemp, Warning, TEXT("Current Direction %f - %f - %f"), Direction.X, Direction.Y, Direction.Z);
 
 		AddMovementInput(Direction, Value);
+
 
 		//AddMovementInput(GetActorForwardVector(), Value);
 	}
@@ -90,6 +91,88 @@ void APlayerCharacter::MoveRight(float Value)
 	}
 }
 
+void APlayerCharacter::MousePitch(float Value)
+{
+	float MaxAnglePitch = 30.f;
+	APlayerController* PC = Cast<APlayerController>(Controller);
+	FRotator WorldControlRotation = GetControlRotation().GetNormalized();
+
+	UE_LOG(LogTemp, Warning, TEXT("World Pitch - %f"), WorldControlRotation.Pitch);
+
+	const float ScaledInput = Value * PC->InputPitchScale;
+	const float PitchSum = WorldControlRotation.Pitch + ScaledInput;
+	if (PitchSum > -MaxAnglePitch && PitchSum < MaxAnglePitch)
+	{
+		PC->RotationInput.Pitch += ScaledInput;
+	}
+	else if (
+		WorldControlRotation.Pitch > -MaxAnglePitch &&
+		WorldControlRotation.Pitch < MaxAnglePitch
+		)
+	{
+		const float InputRemainder = FMath::Sign(ScaledInput) * (MaxAnglePitch - FMath::Abs(WorldControlRotation.Pitch));
+		PC->RotationInput.Pitch += InputRemainder;
+	}
+}
+
+void APlayerCharacter::MouseYaw(float Value)
+{
+	if (Value == 0.0f) return;
+	APlayerController* OwnPlayerController = Cast<APlayerController>(Controller);
+	FRotator WorldControlRotation = GetControlRotation();
+
+	// getting local rotation
+	FRotator LocalControlRotation = FRotator(RootComponent->GetComponentTransform().GetRotation() * WorldControlRotation.Quaternion().Inverse());
+
+	const float ScaledInput = Value * OwnPlayerController->InputYawScale;
+	const float YawSum = LocalControlRotation.Yaw + ScaledInput * -1.f; // why we have to multiply it by -1?
+	if (YawSum > -90.f && YawSum < 90.f)
+	{
+		OwnPlayerController->AddYawInput(Value);
+	}
+	else if (LocalControlRotation.Yaw > -90.f && LocalControlRotation.Yaw < 90.f)
+	{
+		const float InputRemainder = FMath::Sign(Value) * FMath::Abs((90.f - FMath::Abs(LocalControlRotation.Yaw)) / OwnPlayerController->InputYawScale);
+		
+		/*UE_LOG(LogTemp, Warning, TEXT("FMath::Abs(LocalControlRotation.Yaw) - %f"), FMath::Abs(LocalControlRotation.Yaw));
+		UE_LOG(LogTemp, Warning, TEXT("(90.f - FMath::Abs(LocalControlRotation.Yaw)) - %f"), (90.f - FMath::Abs(LocalControlRotation.Yaw)));
+		UE_LOG(LogTemp, Warning, TEXT("OwnPlayerController->InputYawScale - %f"), OwnPlayerController->InputYawScale);
+		UE_LOG(LogTemp, Warning, TEXT("FMath::Abs((90.f - FMath::Abs(LocalControlRotation.Yaw)) / OwnPlayerController->InputYawScale) - %f"), FMath::Abs((90.f - FMath::Abs(LocalControlRotation.Yaw)) / OwnPlayerController->InputYawScale));
+		UE_LOG(LogTemp, Warning, TEXT("FMath::Sign(Value) - %f"), FMath::Sign(Value));
+
+		UE_LOG(LogTemp, Error, TEXT("InputRemainder %f"), InputRemainder);
+		UE_LOG(LogTemp, Error, TEXT("------------------------------------------------"));*/
+		
+		OwnPlayerController->AddYawInput(InputRemainder);
+	}
+
+	///*UE_LOG(LogTemp, Warning, TEXT("MouseYaw - LocalRotation Before - Yaw %f"), LocalControlRotation.Yaw);
+	//UE_LOG(LogTemp, Error, TEXT("MouseYaw - WorldRotation Before - Yaw %f"), WorldControlRotation.Yaw);*/
+	//
+	//if (OwnPlayerController)
+	//{
+	//	if (LocalControlRotation.Yaw >= -90.0f && LocalControlRotation.Yaw <= 90.0f)
+	//	{
+	//		OwnPlayerController->AddYawInput(Value);
+	//	}
+	//	else
+	//	{
+	//		FRotator ControllerCurrentRotation = LocalControlRotation;
+	//		float ControlledYawValue = FMath::Clamp(ControllerCurrentRotation.Yaw, -85.0f, 85.0f);
+	//		//UE_LOG(LogTemp, Warning, TEXT("MouseYaw - Clamped Value - Yaw %f"), ControlledYawValue);
+	//		
+	//		FRotator NewControllerRotation = FRotator(0.0f, ControlledYawValue, 0.0f);
+	//		FRotator TestRotator = FRotator(RootComponent->GetComponentTransform().GetRotation() * NewControllerRotation.Quaternion().Inverse());
+
+	//		/*UE_LOG(LogTemp, Error, TEXT("MouseYaw - NewControllerRotation %f, %f. %f"), TestRotator.Pitch, TestRotator.Yaw, TestRotator.Roll);*/
+	//		
+	//		OwnPlayerController->SetControlRotation(FRotator(WorldControlRotation.Pitch, TestRotator.Yaw, WorldControlRotation.Roll));
+	//		//OwnPlayerController->SetControlRotation(TestRotator);
+
+	//	}
+	//}
+}
+
 // Called to bind functionality to input
 void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
@@ -98,7 +181,7 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
 	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
 
-	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("Turn", this, &APlayerCharacter::MouseYaw);
+	PlayerInputComponent->BindAxis("LookUp", this, &APlayerCharacter::MousePitch);
 }
 
